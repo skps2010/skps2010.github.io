646b225579ab4e3092efe28f642dd58058eb0c5e
skps2010 <skps2010030908@yahoo.com>
2022-08-17 01:13:00 +0800

    add texts generate from file

---
+++ data/link_generator.py
@@ -0,0 +1,10 @@
+import PyPDF2
+
+for i in range(340, 545):
+    file = open(f'{i}.pdf', 'rb')
+    fileReader = PyPDF2.PdfFileReader(file)
+    for j in range(fileReader.numPages):
+        texts = fileReader.getPage(j).extractText().split('\n')
+        for text in texts:
+            if '【' in  text:
+                print(f'[{text}](data/{i}.pdf)  ')
